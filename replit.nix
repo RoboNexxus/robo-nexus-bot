e894776fea63973f2753802ed43146062cb7e49d
Atharv Mandlavdiya <atharvam682@gmail.com>
2026-02-21 14:40:12 +0530

    Simplify Replit config and nix deps
    Change .replit to run `python3 main.py` instead of `./start.sh`, remove the ignored ports/env entries, and keep the deployment target and ports. Update replit.nix to use `pkgs.python311` and only include `pip`, removing setuptools, wheel, postgresql, gcc, libffi, openssl and the custom PYTHON_LD_LIBRARY_PATH/PYTHONBIN/LANG env block. These changes simplify the Replit environment and startup command.

--- replit.nix
+++ replit.nix
@@ -1,23 +1,6 @@
 { pkgs }: {
   deps = [
-    pkgs.python311Full
+    pkgs.python311
     pkgs.python311Packages.pip
-    pkgs.python311Packages.setuptools
-    pkgs.python311Packages.wheel
-    pkgs.postgresql
-    pkgs.gcc
-    pkgs.libffi
-    pkgs.openssl
   ];
-  
-  env = {
-    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      pkgs.stdenv.cc.cc.lib
-      pkgs.zlib
-      pkgs.libffi
-      pkgs.openssl
-    ];
-    PYTHONBIN = "${pkgs.python311Full}/bin/python3.11";
-    LANG = "en_US.UTF-8";
-  };
 }
